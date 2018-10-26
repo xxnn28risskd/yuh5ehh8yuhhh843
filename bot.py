7885c919ac7fbdb9d98b7dae583c749550b8032a
xxnn28risskd <44174033+xxnn28risskd@users.noreply.github.com>
2018-10-26 16:58:20 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -326,6 +326,12 @@ async def listenst(ctx, *,game):
     else:
     	    await bot.say("```Only bot owner```")    	    
     	    
+	
+	
+	
+	
+	
+	
     	         
 @bot.command(pass_context=True)
 async def status1(ctx):
@@ -338,7 +344,71 @@ async def status1(ctx):
                 
     else:
                 await bot.say("```Only bot owner```")
-                
+		
+@bot.command(pass_context = True, aliases=['rr','ro'])
+async def russianroulette(ctx, user1: discord.User=None, user2: discord.User=None, user3: discord.User=None, user4: discord.User=None, user5: discord.User=None, user6: discord.User=None, user7: discord.User=None):
+	reponses = ["{ctx.message.author)", "{user1}","{user2}", "{user3}"]	
+	select = random.choice(reponses) 
+	if select == "{ctx.message.author)":
+		embed = discord.Embed(title="Russian Roulette", colour=(0x36393E))
+		embed.add_field(name="Game", value=f"{user1} Survived!", inline=True)
+		await bot.say(embed=embed)	
+		await asyncio.sleep(3)
+		embed = discord.Embed(title="Russian Roulette", colour=(0x36393E))
+		users0 = ctx.message.author		
+		embed.add_field(name="Game Started", value=f"{ctx.message.author} shot and dead 🔫", inline=True)
+		await bot.say(embed=embed)		
+	if select == "{user3)":		
+		embed = discord.Embed(title="Russian Roulette", colour=(0x36393E))
+		embed.add_field(name="Game", value=f"{user2} Survived!", inline=True)
+		await bot.say(embed=embed)	
+		await asyncio.sleep(3)
+		embed = discord.Embed(title="Russian Roulette", colour=(0x36393E))
+		embed.add_field(name="Game", value=f"{ctx.message.author} Survived!", inline=True)
+		await bot.say(embed=embed)
+		await asyncio.sleep(3)		
+		embed = discord.Embed(title="Russian Roulette", colour=(0x36393E))
+		embed.add_field(name="Game", value=f"{user1} Survived!", inline=True)
+		await bot.say(embed=embed)
+		await asyncio.sleep(4)		
+		embed = discord.Embed(title="Russian Roulette", colour=(0x36393E))
+		users0 = ctx.message.author		
+		embed.add_field(name="Game Started", value=f"{user3} shot and dead 🔫", inline=True)
+		await bot.say(embed=embed)		
+	if select == "{user2)":		
+		embed = discord.Embed(title="Russian Roulette", colour=(0x36393E))
+		embed.add_field(name="Game", value=f"{user3} Survived!", inline=True)
+		await bot.say(embed=embed)	
+		await asyncio.sleep(3)
+		embed = discord.Embed(title="Russian Roulette", colour=(0x36393E))
+		embed.add_field(name="Game", value=f"{ctx.message.author} Survived!", inline=True)
+		await bot.say(embed=embed)
+		await asyncio.sleep(3)		
+		embed = discord.Embed(title="Russian Roulette", colour=(0x36393E))
+		embed.add_field(name="Game", value=f"{user1} Survived!", inline=True)
+		await bot.say(embed=embed)
+		await asyncio.sleep(4)		
+		embed = discord.Embed(title="Russian Roulette", colour=(0x36393E))
+		users0 = ctx.message.author		
+		embed.add_field(name="Game Started", value=f"{user2} shot and dead 🔫", inline=True)
+		await bot.say(embed=embed)
+	if select == "{user1)":		
+		embed = discord.Embed(title="Russian Roulette", colour=(0x36393E))
+		embed.add_field(name="Game", value=f"{user3} Survived!", inline=True)
+		await bot.say(embed=embed)	
+		await asyncio.sleep(3)
+		embed = discord.Embed(title="Russian Roulette", colour=(0x36393E))
+		embed.add_field(name="Game", value=f"{user2} Survived!", inline=True)
+		await bot.say(embed=embed)
+		await asyncio.sleep(3)		
+		embed = discord.Embed(title="Russian Roulette", colour=(0x36393E))
+		embed.add_field(name="Game", value=f"{ctx.message.author} Survived!", inline=True)
+		await bot.say(embed=embed)
+		await asyncio.sleep(4)		
+		embed = discord.Embed(title="Russian Roulette", colour=(0x36393E))
+		users0 = ctx.message.author		
+		embed.add_field(name="Game Started", value=f"{user1} shot and dead 🔫", inline=True)
+		await bot.say(embed=embed)              
 @bot.command(pass_context=True)
 async def hack(ctx, hack: discord.User=None):	
    if hack is None:
@@ -875,7 +945,7 @@ async def h(ctx):
          	await bot.say(embed=em)  
          elif str(reaction.reaction.emoji) == "🍹":		
                 em = discord.Embed(colour=discord.Colour.purple()) 
-                em.add_field(name='Fun Commands',value="`k!dice`, `k!rateidiot`, `k!rate`, `k!anonim_msg`, `k!say`, `k!hug`, `k!spacefont`, `k!reminder`, `emojisteal`", inline=True)
+                em.add_field(name='Fun Commands',value="`k!dice`, `k!rateidiot`, `k!rate`, `k!anonim_msg`, `k!say`, `k!hug`, `k!spacefont`, `k!reminder`, `emojisteal` , `k!russianroullete",inline=True)
                 message = await bot.say(embed=em)
          elif str(reaction.reaction.emoji) == "💾":
                 emb =  discord.Embed(colour=discord.Colour.magenta()) 
